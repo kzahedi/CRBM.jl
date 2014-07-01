de3c36034c9f3b616d1dd611604b6f57ae47d229
Keyan Ghazi-Zahedi <keyan.zahedi@googlemail.com>
2014-07-01 15:31:06 +0200

    Test file
    Added some lines for the development

--- src/test.jl
+++ src/test.jl
@@ -1,4 +1,5 @@
 using CRBM
+#using PyPlot
 
 function run(S, A, bins, m, epochs, batch, alpha, uditer, momentum, weightcost, nr_of_sensors, perturbation)
   println("started job $oindex/$all with $bins $m $epochs $batch $alpha $uditer $weightcost $momentum $nr_of_sensors $perturbation")
@@ -19,4 +20,32 @@ function run(S, A, bins, m, epochs, batch, alpha, uditer, momentum, weightcost, 
   end
   rbm_write("rbm-m_$m/rbm_$index.rbm", rbm)
   println("finished job rbm-m_$m/rbm_$index.rbm on $m")
+  rbm
 end
+
+data      = readdlm("data.log")
+sensors   = [1  2  3  4  5  6  7  8  9  10 11 12]
+actuators = [58 59 60 61 62 63 64 65 66 67 68 69]
+S         = data[:,sensors]
+A         = data[:,actuators]
+
+bins          =
+m             =
+epochs        =
+batch         =
+alpha         =
+uditer        =
+momentum      =
+weightcost    =
+nr_of_sensors =
+perturbation  =
+
+# DAS IST DIE FUNKTION
+rbm = run(S, A, bins, m, epochs, batch, alpha, uditer, momentum, weightcost, nr_of_sensors, perturbation)
+
+println(rbm.W)
+
+#imshow(rbm.W)
+#imshow(rbm.V)
+#plot(rbm.b)
+#plot(rbm.c)
