25bc12f9ce4f6389f614b5e22605fac3546746e5
Keyan Ghazi-Zahedi <keyan.zahedi@googlemail.com>
2014-09-16 19:17:06 +0200

    Refactoring
    - Added dependency on Shannon for the binning functions
    - Removed data.log
    - Removed run.jl
    - Added test cases for the CRBM binning, which builds upon the binning
    of the Shannon package

--- src/CRBM.jl
+++ src/CRBM.jl
@@ -1,32 +1,54 @@
 module CRBM
 
 using RBM
+using ProgressMeter
+using PyPlot
+using Shannon
 
 export crbm_control_sample!
 export crbm_binary_train!
 
 #export from RBM package, so that you don't have to import both
 export RBM_t
+export CRBM_cfg_t, crbm_create_config
 export rbm_copy
 export rbm_create
 export rbm_write, rbm_read
-export binarise_matrix
+export binarise_matrix, i2b, iv2b, bin_value, bin_matrix
+
+type CRBM_cfg_t
+  use_progress_meter::Bool
+  use_pyplot::Bool
+end
+
+function crbm_create_config()
+  return CRBM_cfg_t(true, true)
+end
+
+# 
+# start - covered by test cases
+#
+i2b(v::Int64, n::Int64)          = [(((1 << (n-i)) & v)>0)?1.0:0.0 for i=1:n]
+iv2b(v::Vector{Int64}, n::Int64) = foldl(vcat, [i2b(u ,n) for u in v])
+function binarise_matrix(A::Matrix{Float64}, bins::Int64)
+  N = int(ceil(log2(bins)))
+  B = zeros(size(A)[1], size(A)[2]* N)
+  C = bin_matrix(A, -1.0, 1.0, bins) .- 1
+  for row_index = 1:size(A)[1]
+    B[row_index,:] = iv2b(squeeze(C[row_index,:],1), N)
+  end
+  B
+end
+#
+# end   - covered by test cases
+# 
 
 sigm(p::Matrix{Float64})                                    = 1./(1 .+ exp(-p))
 binary_draw(p::Matrix{Float64})                             = p .> rand(size(p))
 binary_up(rbm::RBM_t, y::Array{Float64}, x::Array{Float64}) = convert(Matrix{Float64},binary_draw(up(rbm, y, x)))
 binary_down(rbm::RBM_t, z::Array{Float64})                  = convert(Matrix{Float64},binary_draw(down(rbm, z)))
-discretise_value(v::Float64, nr_of_bins::Int64)             = int(min(floor(nr_of_bins * (v .+ 1) ./ 2.0), nr_of_bins-1))
-
-function down(rbm::RBM_t, z::Array{Float64})
-  r = sigm(repmat(rbm.b, 1, size(z)[1]) + rbm.W' * z')
-  r'
-end
-
-function up(rbm::RBM_t, y::Array{Float64}, x::Array{Float64})
-  r = sigm(repmat(rbm.c, 1, size(y)[1]) + rbm.V * y' + rbm.W * x')
-  r'
-end
+down(rbm::RBM_t, z::Array{Float64})                         = transpose(sigm(repmat(rbm.b, 1, size(z)[1]) + rbm.W' * z'))
+up(rbm::RBM_t, y::Array{Float64}, x::Array{Float64})        = transpose(sigm(repmat(rbm.c, 1, size(y)[1]) + rbm.V * y' + rbm.W * x'))
 
 function crbm_learn_sampling!(rbm::RBM_t, y::Array{Float64}, X::Array{Float64})
   Z = binary_up(rbm, y, X)
@@ -49,46 +71,27 @@ function crbm_control_sample!(rbm::RBM_t, y::Array{Float64}, X::Array{Float64})
   return X
 end
 
-function int2binary(v::Int64, n::Int64) # checked
-  r=zeros(n)
-  for i=1:n
-    r[i] = (((1 << (n-i)) & v)>0)?1.0:0.0
-  end
-  return r
-end
 
-function binarise_matrix(A::Matrix{Float64}, bins::Int64)
-  N = int(ceil(log2(bins)))
-  B=zeros(size(A)[1], size(A)[2]* N)
-  for i=1:size(A)[1]
-    for j=1:size(A)[2]
-      value = A[i,j]
-      d     = discretise_value(value, bins)
-      b     = int2binary(d, N)
-      for u = 1:N
-        B[i,(j-1)*N+u] = float64(b[u])
-      end
-    end
-  end
-  B
-end
-
-function crbm_binary_train!(rbm, S, A)
+function crbm_binary_train!(cfg::CRBM_cfg_t, rbm::RBM_t, S::Matrix{Float64}, A::Matrix{Float64})
   @assert (rbm.dropout >= 0.0 && rbm.dropout <= 1.0) "Dropout must be in [0,1]"
   # TODO more asserts
   N  = ceil(log2(rbm.bins))
   ss = binarise_matrix(S, rbm.bins)
   aa = binarise_matrix(A, rbm.bins)
+  ns = size(ss[1,:])[1]
 
   if maximum(rbm.W) == 0.0 && minimum(rbm.W) == 0.0 && maximum(rbm.V) == 0.0 && minimum(rbm.V) == 0.0
-    println("Initialising W,V, and c.")
+    println("Initialising W, V, and c.")
     rbm_init_weights_random!(rbm)
-    rbm.c           = zeros(rbm.m)
+    rbm.c = zeros(rbm.m)
   end
 
   binary_s_matrix = binarise_matrix(S, rbm.bins)
   rbm_init_visible_bias!(rbm, convert(Array{Int64},binary_s_matrix))
 
+  if cfg.use_progress_meter == true
+    pm = Progress(rbm.numepochs, 1)
+  end
   for t=1:rbm.numepochs
     # extract data batch for current epoch
     m     = size(ss)[1] - rbm.batchsize
@@ -105,8 +108,8 @@ function crbm_binary_train!(rbm, S, A)
 
     Eb  = transpose(mean(a,1) - mean(A,1))
     Ec  = transpose(mean(z,1) - mean(Z,1))
-    EW  = (z' * a - Z' * A)/size(s)[1]
-    EV  = (z' * s - Z' * s)/size(s)[1]
+    EW  = (z' * a - Z' * A)/ns
+    EV  = (z' * s - Z' * s)/ns
 
     Eb = squeeze(Eb,2)
     Ec = squeeze(Ec,2)
@@ -117,10 +120,11 @@ function crbm_binary_train!(rbm, S, A)
     rbm.V = rbm.V + rbm.alpha * EV
 
     if rbm.momentum > 0.0
-      rbm.b = rbm.b + (rbm.alpha * rbm.momentum) * rbm.vb
-      rbm.c = rbm.c + (rbm.alpha * rbm.momentum) * rbm.vc
-      rbm.W = rbm.W + (rbm.alpha * rbm.momentum) * rbm.vW
-      rbm.V = rbm.V + (rbm.alpha * rbm.momentum) * rbm.vV
+      f = rbm.alpha * rbm.momentum
+      rbm.b = rbm.b + f * rbm.vb
+      rbm.c = rbm.c + f * rbm.vc
+      rbm.W = rbm.W + f * rbm.vW
+      rbm.V = rbm.V + f * rbm.vV
     end
 
     if rbm.weightcost > 0.0 # using L2
@@ -133,6 +137,17 @@ function crbm_binary_train!(rbm, S, A)
     rbm.vW = EW
     rbm.vV = EV
 
+    if cfg.use_pyplot == true
+      if t % 500 == 0 || t == 0
+        clf()
+        subplot(121)
+        colorbar(imshow(rbm.W))
+        subplot(122)
+        colorbar(imshow(rbm.V))
+      end
+    end
+
+    next!(pm)
   end # training iteration
 end
 
--- test/runtests.jl
+++ test/runtests.jl
@@ -3,3 +3,28 @@ using Base.Test
 
 # write your own tests here
 @test 1 == 1
+
+# test basic functions - integer to binary vector
+@test [0.0, 0.0, 0.0, 0.0] == i2b(0 ,4)
+@test [0.0, 0.0, 0.0, 1.0] == i2b(1 ,4)
+@test [0.0, 0.0, 1.0, 0.0] == i2b(2 ,4)
+@test [0.0, 0.0, 1.0, 1.0] == i2b(3 ,4)
+@test [0.0, 1.0, 0.0, 0.0] == i2b(4 ,4)
+@test [0.0, 1.0, 0.0, 1.0] == i2b(5 ,4)
+@test [0.0, 1.0, 1.0, 0.0] == i2b(6 ,4)
+@test [0.0, 1.0, 1.0, 1.0] == i2b(7 ,4)
+@test [1.0, 0.0, 0.0, 0.0] == i2b(8 ,4)
+@test [1.0, 0.0, 0.0, 1.0] == i2b(9 ,4)
+@test [1.0, 0.0, 1.0, 0.0] == i2b(10,4)
+@test [1.0, 0.0, 1.0, 1.0] == i2b(11,4)
+@test [1.0, 1.0, 0.0, 0.0] == i2b(12,4)
+@test [1.0, 1.0, 0.0, 1.0] == i2b(13,4)
+@test [1.0, 1.0, 1.0, 0.0] == i2b(14,4)
+@test [1.0, 1.0, 1.0, 1.0] == i2b(15,4)
+
+# test basic functions - integer vector to binary vector
+@test [0.0, 0.0, 0.0, 0.0] == iv2b([0, 0], 2)
+@test [0.0, 1.0, 1.0, 0.0] == iv2b([1, 2], 2)
+
+# test basic functions - float matrix to binary vector
+@test [[0.0 0.0 0.0 0.0], [0.0 0.0 0.0 0.0]] == binarise_matrix([[-1.0 -1.0], [-1.0 -1.0]], 4)
